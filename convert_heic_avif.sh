9193d83e77f739c2ef84030dfc1c107da6a76fb8
anyonsci <40672737+anyonsci@users.noreply.github.com>
2025-06-02 09:40:03 +0530

    Update convert_heic_avif.sh

--- convert_heic_avif.sh
+++ convert_heic_avif.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 IFS=$'\n'
-files=$(find . -name '*.heic') 
+ext=HEIC
+files=$(find . -name "*.${ext}") 
 
 for file in $files
 do
   ## auto-orient if the file has orientation data or is JPG format.
-  # mogrify -auto-orient "$file"
+  mogrify -auto-orient "$file"
   ## print current file being processed
   # echo $file
-  magick "$file" "${file%.heic}.avif"
+  magick "$file" "${file%.${ext}}.avif"
   rm "$file"
 done
